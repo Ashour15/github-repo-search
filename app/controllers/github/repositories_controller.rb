bc12cef11cd88a4b27f03c6a850c3c664125daff
Ashour15 <mohamedashour15@hotmail.com>
2022-02-27 02:47:08 +0200

    Autload lib files, add next and previous button and modify controller to show real data

--- app/controllers/github/repositories_controller.rb
+++ app/controllers/github/repositories_controller.rb
@@ -1,6 +1,8 @@
 class Github::RepositoriesController < ApplicationController
   def index
+    @page = 1
     @repositories = Github::Repository.search_repositories(params[:query])
+    @repositories = @repositories['items']
     respond_to do |format|
       format.js
     end
