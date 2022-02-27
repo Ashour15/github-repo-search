06da77ec780d3c78c85aef0d921ad3306282eb5b
Ashour15 <mohamedashour15@hotmail.com>
2022-02-27 02:06:18 +0200

    Add views for home and search results table, add table styles using javascript render search results data on homepage table

--- app/controllers/github/repositories_controller.rb
+++ app/controllers/github/repositories_controller.rb
@@ -1,5 +1,8 @@
 class Github::RepositoriesController < ApplicationController
   def index
     @repositories = Github::Repository.search_repositories(params[:query])
+    respond_to do |format|
+      format.js
+    end
   end
 end
